0f1865dacd94b64f3b3114a1e1ea52ba7cf301f4
Just Playin <ensminger@yahoo.com>
2024-07-02 18:54:50 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,9 +5,26 @@ echo "This script will install docker and portainer"
 echo "Press enter to begin or ctrl+c to exit"
 read junk
 
-sudo apt install -y docker.io docker-compose
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo service docker start
+sudo service docker enable
 sleep 3
 sudo usermod -aG docker $USER
 sudo docker volume create portainer_data
