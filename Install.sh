ff778b2c7c9661f5a5210b43cde19304eabafe6b
Just Playin <ensminger@yahoo.com>
2025-08-02 13:43:02 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,27 +6,44 @@ echo "Press enter to begin or ctrl+c to exit"
 read junk
 
 # Update the system
+echo "Updating the system"
+sleep 2
 sudo apt-get update
 sudo apt install -y curl
 
 # Install Docker
+clear
+echo "Installing Docker"
+sleep 2
 curl -fsSL https://get.docker.com -o get-docker.sh
-exit
-#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+bash get-docker.sh
+
+# Enable and start Docker
+clear
+echo "Enabling and starting docker"
+sleep 2
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo service docker start
 sudo service docker enable
-sleep 3
 sudo usermod -aG docker $USER
+
+# Install portainer and watchtower
+clear
+echo "Install portainer and watchtower"
+sleep 2
 sudo docker volume create portainer_data
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 clear
 sudo docker run  -v /var/run/docker.sock:/var/run/docker.sock --name upgrade-images containrrr/watchtower --run-once
+
+# Check Containers
 clear
 echo "We will now check running docker containers"
 sudo docker ps
-echo \n\n\n
+echo ""
+echo ""
+echo ""
 if [ $? == 0];
   echo "Docker Install Successful"
 else:
